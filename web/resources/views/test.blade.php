df5f289b7112051353320b33f764d73c1491a890
Kaenova Mahendra Auditama <kaenova@gmail.com>
2022-03-07 10:53:21 +0700

    Init Repo

---
+++ web/resources/views/test.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Testing Tailwind</title>
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="bg-black h-screen w-screen flex flex-col items-center justify-center">
+<div class="alert alert-info shadow-lg">
+  <div>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current flex-shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+    <span>Halo gais ini pake daisy UI juga yaa.</span>
+  </div>
+</div>
+</body>
+</html>
