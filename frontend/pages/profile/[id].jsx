d75b39c6dafc62f350ebb742ea0ccf967432c7d5
fanzru <fattahilaaf080701@gmail.com>
2022-04-02 21:47:56 +0700

    fix bug view login

--- frontend/pages/profile/[id].jsx
+++ frontend/pages/profile/[id].jsx
@@ -27,12 +27,7 @@ export default function ProfilSendiri() {
             </div>
           </div>
         </div>
-        
-        localhost:3000/profile/10
-        
-        buka profil orang lainan
-
-        localhost:3000/myprofile
+      
 
         {/* <div className='mt-[4px] flex items-center justify-between h-[50px] p-[12px]'>
           <div className="flex items-center">
