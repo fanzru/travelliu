1c8bc1228b54371caafe075605de7b1442c1feb6
fanzru <fattahilaaf080701@gmail.com>
2022-04-02 21:46:27 +0700

    fix bug view login

--- frontend/pages/myprofile.jsx
+++ frontend/pages/myprofile.jsx
@@ -6,16 +6,14 @@ export default function ProfilSendiri() {
     <div className="flex justify-center">
       <div className="border-2 max-w-[720px] w-full min-h-screen">
         <div className="hero container max-w-screen-lg justify-center pt-10">
-          <img className="object-cover h-[262px] w-[262px] rounded-full" src="/kiseki-no-sedai.jpeg" alt="" />
-
+          <img className="object-cover h-[160px] w-[160px] rounded-full" src="/kiseki-no-sedai.jpeg" alt="" />
         </div>
         <div className="hero container max-w-screen-lg justify-center pb-2 pt-2">
           <p className='text-[37.54px] font-bold'>
             Ananda Fatahilla
           </p>
         </div>
-
-        <div className='flex justify-center mt-20'>
+        <div className='flex justify-center'>
           <div className='w-[350px] grid grid-cols-2'>
             <div className='text-center'>
               <p className='underline'>Total Review</p>
@@ -28,10 +26,7 @@ export default function ProfilSendiri() {
           </div>
         </div>
 
-
-
-
-        {/* <div className='mt-[4px] flex items-center justify-between h-[50px] p-[12px]'>
+        <div className='mt-[4px] flex items-center justify-between h-[50px] p-[12px]'>
           <div className="flex items-center">
             <img className="object-cover h-[35px] w-[35px] rounded-full" src="/kiseki-no-sedai.jpeg" alt="" />
             <p className="mx-2 text-[18px] font-bold">Ananda Affan Fatahilla</p>
@@ -49,7 +44,7 @@ export default function ProfilSendiri() {
         </div>
         <div className='mt-[2px] flex items-center h-[50px] p-[12px]'>
           <p className='text-[14px]'>Negeri ini sangat indah dimana hati, otak dan pikiran akan selalu tenang karena tempat yang dapat membuat semua beban terlepas serta mendapatkan ketenangan jiwa hhe :).</p>
-        </div> */}
+        </div>
 
 
 
--- frontend/pages/profile/[id].jsx
+++ frontend/pages/profile/[id].jsx
@@ -6,7 +6,7 @@ export default function ProfilSendiri() {
     <div className="flex justify-center">
       <div className="border-2 max-w-[720px] w-full min-h-screen">
         <div className="hero container max-w-screen-lg justify-center pt-10">
-          <img className="object-cover h-[160px] w-[160px] rounded-full" src="/kiseki-no-sedai.jpeg" alt="" />
+          <img className="object-cover h-[262px] w-[262px] rounded-full" src="/kiseki-no-sedai.jpeg" alt="" />
 
         </div>
         <div className="hero container max-w-screen-lg justify-center pb-2 pt-2">
@@ -15,7 +15,7 @@ export default function ProfilSendiri() {
           </p>
         </div>
 
-        <div className='flex justify-center'>
+        <div className='flex justify-center mt-20'>
           <div className='w-[350px] grid grid-cols-2'>
             <div className='text-center'>
               <p className='underline'>Total Review</p>
@@ -27,8 +27,14 @@ export default function ProfilSendiri() {
             </div>
           </div>
         </div>
+        
+        localhost:3000/profile/10
+        
+        buka profil orang lainan
 
-        <div className='mt-[4px] flex items-center justify-between h-[50px] p-[12px]'>
+        localhost:3000/myprofile
+
+        {/* <div className='mt-[4px] flex items-center justify-between h-[50px] p-[12px]'>
           <div className="flex items-center">
             <img className="object-cover h-[35px] w-[35px] rounded-full" src="/kiseki-no-sedai.jpeg" alt="" />
             <p className="mx-2 text-[18px] font-bold">Ananda Affan Fatahilla</p>
@@ -46,7 +52,7 @@ export default function ProfilSendiri() {
         </div>
         <div className='mt-[2px] flex items-center h-[50px] p-[12px]'>
           <p className='text-[14px]'>Negeri ini sangat indah dimana hati, otak dan pikiran akan selalu tenang karena tempat yang dapat membuat semua beban terlepas serta mendapatkan ketenangan jiwa hhe :).</p>
-        </div>
+        </div> */}
 
 
 
