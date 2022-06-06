a004fb6e85fc31cb77b5560fd15c581499f39801
Fendi Irfan Amorokhman <amorokhman@gmail.com>
2022-06-06 22:42:25 +0700

    Register and login bux fixed

--- mobile/lib/screen/form_register/register_screen.dart
+++ mobile/lib/screen/form_register/register_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/api/user.dart';
+import 'package:mobile/utils/show_snackbar.dart';
 
 class RegisterScreenArguments {
   RegisterScreenArguments();
@@ -14,11 +16,243 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class RegisterScreenState extends State<RegisterScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _namaController = TextEditingController();
+  final TextEditingController _repasswordController = TextEditingController();
+
+  
+  void _handleKembaliButton() {
+    Navigator.pop(context);
+  }
+
+  String? _validEmail(String? txt){
+    if (txt == null || txt.isEmpty) {
+      return "Masukkan email";
+    }
+    if (!RegExp(
+            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+        .hasMatch(txt)) {
+      return "Email tidak valid";
+    }
+    return null;
+  }
+
+  String? _validPassword(String? txt){
+    if (txt == null || txt.isEmpty) {
+      return "Masukkan password";
+    }
+    if (txt.length < 6) {
+      return "Password minimal 6 karakter";
+    }
+    return null;
+  }
+
+  String? _validRepassword(String? txt){
+    if (txt != _passwordController.text) {
+      return "Password tidak sama";
+    }
+    return null;
+  }
+
+  String? _validNama(String? txt){
+    if (txt == null || txt.isEmpty) {
+      return "Masukkan nama";
+    }
+    return null;
+  }
+  void _handleSubmit() async{
+    try{
+      var respon= 
+        await registerUser(_namaController.text, _emailController.text, _passwordController.text);
+      ShowSnackBar(context, "Akun ${_namaController.text} berhasil dibuat");
+    }catch(err){
+      ShowSnackBar(context, "$err");
+    }
+    
+
+  }
+  
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Center(child: Text("Welcome to Register Screen")),
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      body: Container(
+          child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextButton(
+                      style: ButtonStyle(
+                          overlayColor:
+                              MaterialStateProperty.all(Colors.black12)),
+                      onPressed: _handleKembaliButton,
+                      child: const Text(
+                        "Kembali",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15),
+                      )),
+                  Expanded(
+                      child: Center(
+                          child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Travelliu",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 30),
+                      ),
+                      const Text(
+                        "Register",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 21),
+                      ),
+                      const SizedBox(
+                        height: 25,
+                      ),
+                      Form(
+                          key: _formKey,
+                          child: SingleChildScrollView(
+                            child: Column(
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Text("Nama"),
+                                    TextFormField(
+                                      controller: _namaController,
+                                      validator: _validNama,
+                                      decoration: InputDecoration(
+                                        focusedBorder: OutlineInputBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                            borderSide: const BorderSide(
+                                                color: Colors.black)),
+                                        contentPadding:
+                                            const EdgeInsets.symmetric(
+                                                horizontal: 12),
+                                        border: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Text("Email"),
+                                    TextFormField(
+                                      controller: _emailController,
+                                      validator: _validEmail,
+                                      decoration: InputDecoration(
+                                        focusedBorder: OutlineInputBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                            borderSide: const BorderSide(
+                                                color: Colors.black)),
+                                        contentPadding:
+                                            const EdgeInsets.symmetric(
+                                                horizontal: 12),
+                                        border: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Text("Password"),
+                                    TextFormField(
+                                      controller: _passwordController,
+                                      validator: _validPassword,
+                                      obscureText: true,
+                                      decoration: InputDecoration(
+                                        focusedBorder: OutlineInputBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                            borderSide: const BorderSide(
+                                                color: Colors.black)),
+                                        contentPadding:
+                                            const EdgeInsets.symmetric(
+                                                horizontal: 12),
+                                        border: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Text("Re-Password"),
+                                    TextFormField(
+                                      controller: _repasswordController,
+                                      validator: _validRepassword,
+                                      obscureText: true,
+                                      decoration: InputDecoration(
+                                        focusedBorder: OutlineInputBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                            borderSide: const BorderSide(
+                                                color: Colors.black)),
+                                        contentPadding:
+                                            const EdgeInsets.symmetric(
+                                                horizontal: 12),
+                                        border: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(
+                                  height: 20,
+                                ),
+                                ElevatedButton(
+                                    style: ButtonStyle(
+                                        backgroundColor:
+                                            MaterialStateProperty.all(
+                                                Colors.black)),
+                                    onPressed: _handleSubmit,
+                                    child: const Text(
+                                      "Submit",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold),
+                                    )),
+                                const SizedBox(
+                                  height: 20,
+                                ),
+                              ],
+                            ),
+                          )),
+                    ],
+                  )))
+                ],
+              ))),
     );
   }
 }
