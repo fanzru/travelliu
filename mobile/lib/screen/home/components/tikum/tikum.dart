6afb501e4d5d928003b0a2bd7fb98d625bb8ebe3
Kaenova Mahendra Auditama <kaenova@gmail.com>
2022-06-16 10:11:50 +0700

    Changing Tikum FAB for performance

--- mobile/lib/screen/home/components/tikum/tikum.dart
+++ mobile/lib/screen/home/components/tikum/tikum.dart
@@ -5,21 +5,8 @@ import 'package:mobile/screen/test_screen/test_screen.dart';
 import 'package:mobile/screen/home/components/tikum/global_tikum.dart';
 import 'package:mobile/screen/home/components/tikum/my_tikum.dart';
 
-class TikumLayout extends StatefulWidget {
-  TikumLayout({Key? key}) : super(key: key);
-
-  @override
-  State<TikumLayout> createState() => _TikumLayoutState();
-}
-
-class _TikumLayoutState extends State<TikumLayout> {
-  late Future<SecureProfile> futureProfile;
-
-  @override
-  void initState() {
-    futureProfile = SecureProfile.getStorage();
-    super.initState();
-  }
+class TikumLayout extends StatelessWidget {
+  const TikumLayout({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +14,7 @@ class _TikumLayoutState extends State<TikumLayout> {
         length: 2,
         child: Scaffold(
           backgroundColor: Colors.white,
-          floatingActionButton: plusFloatingBuilder(futureProfile),
+          floatingActionButton: const _FloatingActionButtonTikum(),
           appBar: AppBar(
             backgroundColor: Colors.white,
             flexibleSpace: Column(
@@ -46,29 +33,49 @@ class _TikumLayoutState extends State<TikumLayout> {
           ),
           body: TabBarView(children: [GlobalTikum(), MyTikum()]),
         ));
+    ;
   }
 }
 
-FutureBuilder<SecureProfile> plusFloatingBuilder(Future<SecureProfile> future) {
-  return FutureBuilder<SecureProfile>(
-      future: future,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          // Kalau misalkan logged in
-          if (snapshot.data!.getLoggedInStatus()) {
-            return FloatingActionButton(
-              child: const Icon(Icons.add),
-              heroTag: "Buat Review",
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return const FormTikumScreen();
-                }));
-              },
-              backgroundColor: Colors.black,
-              foregroundColor: Colors.white,
-            );
-          }
-        }
-        return const SizedBox.shrink();
-      });
+class _FloatingActionButtonTikum extends StatefulWidget {
+  const _FloatingActionButtonTikum({Key? key}) : super(key: key);
+
+  @override
+  State<_FloatingActionButtonTikum> createState() =>
+      __FloatingActionButtonTikumState();
+}
+
+class __FloatingActionButtonTikumState
+    extends State<_FloatingActionButtonTikum> {
+  bool isLoggedIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkLoggedIn();
+  }
+
+  void _checkLoggedIn() async {
+    var profile = await SecureProfile.getStorage();
+    setState(() {
+      isLoggedIn = profile.getLoggedInStatus();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return !isLoggedIn
+        ? const SizedBox.shrink()
+        : FloatingActionButton(
+            child: const Icon(Icons.add),
+            heroTag: "Buat Review",
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return const FormTikumScreen();
+              }));
+            },
+            backgroundColor: Colors.black,
+            foregroundColor: Colors.white,
+          );
+  }
 }
